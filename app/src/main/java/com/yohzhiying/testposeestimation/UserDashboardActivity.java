b75afdb086120b2ba91dafa41d833162f41dc079
Yoh Zhi Ying <zhiying038@gmail.com>
2021-03-27 18:30:28 +0800

    add search feature

--- app/src/main/java/com/yohzhiying/testposeestimation/UserDashboardActivity.java
+++ app/src/main/java/com/yohzhiying/testposeestimation/UserDashboardActivity.java
@@ -2,6 +2,7 @@ package com.yohzhiying.testposeestimation;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -13,13 +14,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
@@ -32,10 +33,12 @@ public class UserDashboardActivity extends AppCompatActivity implements Navigati
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     RecyclerView outfitRecyclerView;
+    SearchView searchView;
 
     OutfitAdapter outfitAdapter;
     List<Outfit> outfits;
     FirebaseUser firebaseUser;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +48,34 @@ public class UserDashboardActivity extends AppCompatActivity implements Navigati
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
+        String userPhone = firebaseUser.getPhoneNumber();
+        reference = FirebaseDatabase.getInstance().getReference().child("outfits").child(userPhone);
+
         menuIcon = findViewById(R.id.menu_icon);
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.navigation_view);
         outfitRecyclerView = findViewById(R.id.recycler_view_outfits);
+        searchView = findViewById(R.id.search_outfit);
+
+        if (searchView != null) {
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String s) {
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String s) {
+                    if (!s.isEmpty()) {
+                        searchOutfit(s);
+                    } else {
+                        outfitRecycler();
+                    }
+
+                    return true;
+                }
+            });
+        }
 
         outfitRecycler();
         navigationDrawer();
@@ -70,23 +97,37 @@ public class UserDashboardActivity extends AppCompatActivity implements Navigati
     }
 
     private void getOutfitByPhone() {
-        String userPhone = firebaseUser.getPhoneNumber();
-        assert userPhone != null;
-        FirebaseDatabase.getInstance().getReference().child("outfits").child(userPhone).orderByChild("outfitCategory").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                    Outfit outfitHelper = dataSnapshot.getValue(Outfit.class);
-                    outfits.add(outfitHelper);
+        if (reference != null) {
+            reference.orderByChild("outfitCategory").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                            Outfit outfitHelper = dataSnapshot.getValue(Outfit.class);
+                            outfits.add(outfitHelper);
+                        }
+                        outfitAdapter.notifyDataSetChanged();
+                    }
                 }
-                outfitAdapter.notifyDataSetChanged();
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
 
+                }
+            });
+        }
+    }
+
+    private void searchOutfit(String str) {
+        ArrayList<Outfit> myList = new ArrayList<>();
+        for (Outfit object : myList) {
+            if (object.getOutfitName().toLowerCase().contains(str.toLowerCase())) {
+                myList.add(object);
             }
-        });
+        }
+
+        OutfitAdapter filterOutfitAdapter = new OutfitAdapter(this, myList);
+        outfitRecyclerView.setAdapter(filterOutfitAdapter);
     }
 
     private void navigationDrawer() {
