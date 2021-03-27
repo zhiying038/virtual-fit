52b28f0841916f2a13f7d2119bb397873b486f05
Yoh Zhi Ying <zhiying038@gmail.com>
2021-03-27 14:09:28 +0800

    feat: redirect to camera activity from view outfit

--- app/src/main/java/com/yohzhiying/testposeestimation/ViewOutfitActivity.java
+++ app/src/main/java/com/yohzhiying/testposeestimation/ViewOutfitActivity.java
@@ -1,25 +1,25 @@
 package com.yohzhiying.testposeestimation;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
-public class ViewOutfitActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class ViewOutfitActivity extends AppCompatActivity {
 
-    ImageView menuIcon, outfitImage;
+    ImageView backIcon, outfitImage;
     TextView viewOutfitName, viewOutfitDescription, viewOutfitCategory;
+    String _category, _name, _description, _url;
+    Bitmap outfitBitmap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +27,16 @@ public class ViewOutfitActivity extends AppCompatActivity implements NavigationV
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_view_outfit);
 
-        menuIcon = findViewById(R.id.view_outfit_menu);
+        backIcon = findViewById(R.id.view_outfit_back);
         outfitImage = findViewById(R.id.view_outfit_image);
         viewOutfitCategory = findViewById(R.id.view_outfit_category);
         viewOutfitDescription = findViewById(R.id.view_outfit_description);
         viewOutfitName = findViewById(R.id.view_outfit_name);
 
-        final String _category = getIntent().getStringExtra("itemCategory");
-        final String _name = getIntent().getStringExtra("itemName");
-        final String _description = getIntent().getStringExtra("itemDescription");
-        final String _url = getIntent().getStringExtra("itemUrl");
+        _category = getIntent().getStringExtra("itemCategory");
+         _name = getIntent().getStringExtra("itemName");
+        _description = getIntent().getStringExtra("itemDescription");
+        _url = getIntent().getStringExtra("itemUrl");
 
         Picasso.get().load(_url).into(outfitImage);
         viewOutfitCategory.setText(_category);
@@ -44,29 +44,32 @@ public class ViewOutfitActivity extends AppCompatActivity implements NavigationV
         viewOutfitDescription.setText(_description);
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.nav_profile:
-                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
-                break;
-            case R.id.nav_logout:
-                FirebaseAuth.getInstance().signOut();
-                Toast.makeText(ViewOutfitActivity.this, "You've signed out!", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(getApplicationContext(), AuthStartUpActivity.class));
-                break;
-            case R.id.nav_rate_us:
-                startActivity(new Intent(getApplicationContext(), RateUsActivity.class));
-                finish();
-                break;
-            case R.id.nav_add_outfit:
-                startActivity(new Intent(getApplicationContext(), AddOutfitActivity.class));
-                break;
-            case R.id.nav_home:
-                startActivity(new Intent(getApplicationContext(), UserDashboardActivity.class));
-                break;
-        }
+    public void backHome(View view) {
+        startActivity(new Intent(getApplicationContext(), UserDashboardActivity.class));
+        finish();
+    }
 
-        return true;
+    public void onPressTry(View view) {
+        Picasso.get().load(_url).into(new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                outfitBitmap = bitmap;
+            }
+
+            @Override
+            public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+            }
+        });
+        String itemCategory = _category;
+        String itemName = _name;
+        DrawView.currentOutfit = new Outfit(itemCategory, itemName, outfitBitmap);
+        Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
+        startActivity(intent);
     }
 }
