939f9ae91c080ce7ffec2954d9c69b4378dfcb8d
Yoh Zhi Ying <zhiying038@gmail.com>
2021-03-29 20:57:40 +0800

    convert sign up screen to user email and password

--- app/src/main/java/com/yohzhiying/testposeestimation/SignUp3rdClassActivity.java
+++ app/src/main/java/com/yohzhiying/testposeestimation/SignUp3rdClassActivity.java
@@ -4,24 +4,24 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActivityOptions;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.util.Pair;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 import com.hbb20.CountryCodePicker;
 
 public class SignUp3rdClassActivity extends AppCompatActivity {
@@ -31,11 +31,15 @@ public class SignUp3rdClassActivity extends AppCompatActivity {
     CountryCodePicker countryCodePicker;
     RelativeLayout progressbar;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sign_up3rd_class);
+
+        mAuth = FirebaseAuth.getInstance();
 
         scrollView = findViewById(R.id.signup_3rd_screen_scroll_view);
         countryCodePicker = findViewById(R.id.country_code_picker);
@@ -52,7 +56,7 @@ public class SignUp3rdClassActivity extends AppCompatActivity {
 
         if (!validatePhoneNumber()) {
             return;
-        }//Validation succeeded and now move to next screen to verify phone number and save data
+        }
         progressbar.setVisibility(View.VISIBLE);
 
         final String _fullName = getIntent().getStringExtra("fullName");
@@ -62,46 +66,34 @@ public class SignUp3rdClassActivity extends AppCompatActivity {
         final String _date = getIntent().getStringExtra("date");
         final String _gender = getIntent().getStringExtra("gender");;
 
-        String _getUserEnteredPhoneNumber = phoneNumber.getEditText().getText().toString().trim(); //Get Phone Number
+        String _getUserEnteredPhoneNumber = phoneNumber.getEditText().getText().toString().trim();
         if (_getUserEnteredPhoneNumber.charAt(0) == '0') {
             _getUserEnteredPhoneNumber = _getUserEnteredPhoneNumber.substring(1);
-        } //remove 0 at the start if entered by the user
+        }
         final String _phoneNo = "+" + countryCodePicker.getFullNumber() + _getUserEnteredPhoneNumber;
 
-        //Check whether User exists or not in database
-        Query checkUser = FirebaseDatabase.getInstance().getReference("users").orderByChild("phoneNo").equalTo(_phoneNo);
-        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+        Sessions sessionManager = new Sessions(getApplicationContext(), Sessions.SESSION_USERSESSION);
+        sessionManager.createLoginSession(_fullName, _username, _email, _phoneNo, _password, _date, _gender);
+
+        mAuth.createUserWithEmailAndPassword(_email, _password).addOnCompleteListener(SignUp3rdClassActivity.this, new OnCompleteListener<AuthResult>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()) {
-                    phoneNumber.setError("Phone number has already registered!");
-                    progressbar.setVisibility(View.GONE);
-                    return;
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                FirebaseDatabase rootNode = FirebaseDatabase.getInstance();
+                DatabaseReference reference = rootNode.getReference("users");
+
+                if (task.isSuccessful()) {
+                    Log.d("Signup", "createUserWithEmail:success");
+                    UserHelper addNewUser = new UserHelper(_fullName, _username, _email, _phoneNo, _password, _date, _gender);
+
+                    String userUid = task.getResult().getUser().getUid();
+
+                    reference.child(userUid).setValue(addNewUser);
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                    finish();
                 } else {
-                    phoneNumber.setError(null);
-                    phoneNumber.setErrorEnabled(false);
-
-                    Intent intent = new Intent(getApplicationContext(), VerifyOtpActivity.class);
-                    intent.putExtra("fullName", _fullName);
-                    intent.putExtra("email", _email);
-                    intent.putExtra("username", _username);
-                    intent.putExtra("password", _password);
-                    intent.putExtra("date", _date);
-                    intent.putExtra("gender", _gender);
-                    intent.putExtra("phoneNo", _phoneNo);
-                    intent.putExtra("whatToDo", "createNewUser");
-
-                    Pair[] pairs = new Pair[1];
-                    pairs[0] = new Pair<View, String>(scrollView, "transition_OTP_screen");
-                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SignUp3rdClassActivity.this, pairs);
-                    startActivity(intent, options.toBundle());
-                    progressbar.setVisibility(View.GONE);
+                    Log.d("Signup", "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(SignUp3rdClassActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
                 }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(SignUp3rdClassActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
                 progressbar.setVisibility(View.GONE);
             }
         });
